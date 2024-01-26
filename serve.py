461d439da683ae3b1e3defb6715df7792d508e59
Suraj Budhathoki <sbudhathoki789@gmail.com>
2024-01-26 17:21:25 -0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -14,7 +14,7 @@ def get_privateip():
 def call_stress_cpu():
    script_path = os.path.dirname(os.path.realpath(__file__))
    os.chdir(script_path)
-   subprocess.Popen(['python', 'stress_cpu.py'])
+   subprocess.Popen(['python3', 'stress_cpu.py'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
